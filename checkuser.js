f08ac2b9f69d49ab7142f9b0c744784649b05d09
Your Name <you@example.com>
2025-10-13 21:58:31 +0500

    update

---
+++ checkuser.js
@@ -0,0 +1,13 @@
+// checkUser.js
+window.addEventListener('DOMContentLoaded', () => {
+  const user = JSON.parse(localStorage.getItem('biteboxUser'));
+  const isLoggedIn = localStorage.getItem('isLoggedIn');
+
+  if (!user) {
+    window.location.href = 'register.html';
+  } else if (!isLoggedIn) {
+    window.location.href = 'login.html';
+  } else {
+    window.location.href = 'index.html';
+  }
+});
